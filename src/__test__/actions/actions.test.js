13e00b51699a6ff7f3b56ce570aaa99a295f83f4
Juan Camilo Garcés <jgarces@sophilabs.com>
2021-12-20 11:31:34 -0500

    tests

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,17 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions', () => {
+  const payload = ProductMock;
+  const expected = action => ({
+    type: action,
+    payload,
+  });
+  test('addToCart Action', () => {
+    expect(actions.addToCart(payload)).toEqual(expected('ADD_TO_CART'));
+  });
+
+  test('removeFromCart Action', () => {
+    expect(actions.removeFromCart(payload)).toEqual(expected('REMOVE_FROM_CART'));
+  });
+});
